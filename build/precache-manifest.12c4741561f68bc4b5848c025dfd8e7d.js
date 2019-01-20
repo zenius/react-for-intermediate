899166ecb072bcd97bb54946526b81f8c01dbd2a
zenius <zenius2050@gmail.com>
2019-01-20 13:57:59 +0530

    .gitignore file modified

---
+++ build/precache-manifest.12c4741561f68bc4b5848c025dfd8e7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5c0436d55f12af99760ca65640d19d19",
+    "url": "/static/media/logo.5c0436d5.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ac94176a9338dba09f91",
+    "url": "/static/js/main.ac94176a.chunk.js"
+  },
+  {
+    "revision": "ab98e4ae612017cee313",
+    "url": "/static/js/1.ab98e4ae.chunk.js"
+  },
+  {
+    "revision": "ac94176a9338dba09f91",
+    "url": "/static/css/main.c3ea5461.chunk.css"
+  },
+  {
+    "revision": "7077d9af1dc6a25787851d1818ebfd46",
+    "url": "/index.html"
+  }
+];
